58a61dd8c5284c648a85f12745972618b4b1e3a9
Kimia Ketabforoosh <keam96@ymail.com>
2025-03-25 17:30:12 -0400

    updated

--- hash.py
+++ hash.py
@@ -3,6 +3,8 @@ Created on Monday March 24 14:25:41 2025
 @author: Kimia Ketabforoosh
 """
 
+import hashlib
+
 # -----hash table-----
 # measure ASCII code
 names = ["Kimia", "Sarah", "Alison", "Marcus", "Albert"]
@@ -16,4 +18,20 @@ for name in names:
 
 
 # -----SHA256 Brute Force-----
-import hashlib
+m = hashlib.sha256()
+data = "my name is Kimia and 123456 is my student ID"
+
+
+print(hash[-4:])
+
+i = 0
+while True:
+    data = data + str(i)
+    m = m.update(data.encode("utf.8"))
+    hash = m.hexdigest()
+
+    if hash[-4:] == "1111":
+        print(f"Found it! the string is {data} and the hash is {hash}")
+    else:
+        print(hash)
+        break
