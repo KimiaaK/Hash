0084c6e24ab23e9eb80d9b86c5da693007ea6cd4
Kimia Ketabforoosh <keam96@ymail.com>
2025-03-25 17:08:46 -0400

    started the exercise

--- hash.py
+++ hash.py
@@ -3,6 +3,7 @@ Created on Monday March 24 14:25:41 2025
 @author: Kimia Ketabforoosh
 """
 
+# -----hash table-----
 # measure ASCII code
 names = ["Kimia", "Sarah", "Alison", "Marcus", "Albert"]
 for name in names:
@@ -11,3 +12,8 @@ for name in names:
         hash += ord(c)
 
     print(name, hash % 10)
+# Still not the best option for the hash because it might group people in the same group.
+
+
+# -----SHA256 Brute Force-----
+import hashlib
