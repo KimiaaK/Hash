7103fb390c38cacd67038f08bc4f183c0136a8ae
Kimia Ketabforoosh <keam96@ymail.com>
2025-03-26 10:04:45 -0400

    fixed errors

--- hash.py
+++ hash.py
@@ -21,16 +21,30 @@ for name in names:
 m = hashlib.sha256()
 data = "my name is Kimia and 123456 is my student ID"
 
+print("-----------------------------------")
 
 i = 0
 while True:
-    data = data + str(i)
-    m = m.update(data.encode("utf-8"))
+    current_data = data + str(i)
+    m = hashlib.md5()  # create a new hash object each time
+    m.update(current_data.encode("utf-8"))  # do NOT assign m = m.update()
     hash = m.hexdigest()
-    print(hash[-4:])
+    # print(hash[-4:])
 
     if hash[-4:] == "1111":
-        print(f"Found it! the string is {data} and the hash is {hash}")
-    else:
-        print(hash)
+        print(f"Found it! The string is {current_data} and the hash is {hash}")
         break
+    else:
+        i += 1  # keep searching
+# i = 0
+# while True:
+#     data = data + str(i)
+#     m = m.update(data.encode("utf-8"))
+#     hash = m.hexdigest()
+#     print(hash[-4:])
+
+#     if hash[-4:] == "1111":
+#         print(f"Found it! the string is {data} and the hash is {hash}")
+#     else:
+#         print(hash)
+#         break
